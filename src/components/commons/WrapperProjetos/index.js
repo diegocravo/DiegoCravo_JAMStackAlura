954265c3076ec8a1abf775ac0179fb9587fbf70c
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-02-24 08:17:21 -0300

    updates

--- src/components/commons/WrapperProjetos/index.js
+++ src/components/commons/WrapperProjetos/index.js
@@ -18,65 +18,67 @@ const WrapperProjetos = () => {
         flexDirection="column"
       >
         <Text as="h1" variant="title">
-          Meus Projetos
+          MEUS PROJETOS
         </Text>
       </Grid.Col>
 
-      <Grid.Row>
-        <Grid.Col
-          value={{ xs: 12, md: 6, lg: 4 }}
-          offset={{ xs: 0, md: 0, lg: 0 }}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexDirection="column"
-        >
-          <Card.Container>
-            <Card.Img src="https://via.placeholder.com/300x350" />
-            <Card.CardText>
-              <Text as="h2" variant="paragraph1" textAlign={"center"}>
-                PROJETO
-              </Text>
-            </Card.CardText>
-          </Card.Container>
-        </Grid.Col>
+      <div style={{ marginBottom: "32px" }}>
+        <Grid.Row>
+          <Grid.Col
+            value={{ xs: 12, md: 6, lg: 4 }}
+            offset={{ xs: 0, md: 0, lg: 0 }}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <Card.Container>
+              <Card.Img src="https://via.placeholder.com/300x350" />
+              <Card.CardText>
+                <Text as="h2" variant="paragraph1" textAlign={"center"}>
+                  PROJETO
+                </Text>
+              </Card.CardText>
+            </Card.Container>
+          </Grid.Col>
 
-        <Grid.Col
-          value={{ xs: 12, md: 6, lg: 4 }}
-          offset={{ xs: 0, md: 0, lg: 0 }}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexDirection="column"
-        >
-          <Card.Container>
-            <Card.Img src="https://via.placeholder.com/300x350" />
-            <Card.CardText>
-              <Text as="h2" variant="paragraph1" textAlign={"center"}>
-                PROJETO
-              </Text>
-            </Card.CardText>
-          </Card.Container>
-        </Grid.Col>
+          <Grid.Col
+            value={{ xs: 12, md: 6, lg: 4 }}
+            offset={{ xs: 0, md: 0, lg: 0 }}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <Card.Container>
+              <Card.Img src="https://via.placeholder.com/300x350" />
+              <Card.CardText>
+                <Text as="h2" variant="paragraph1" textAlign={"center"}>
+                  PROJETO
+                </Text>
+              </Card.CardText>
+            </Card.Container>
+          </Grid.Col>
 
-        <Grid.Col
-          value={{ xs: 12, md: 6, lg: 4 }}
-          offset={{ xs: 0, md: 0, lg: 0 }}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          flexDirection="column"
-        >
-          <Card.Container>
-            <Card.Img src="https://via.placeholder.com/300x350" />
-            <Card.CardText>
-              <Text as="h1" variant="paragraph1" textAlign={"center"}>
-                PROJETO
-              </Text>
-            </Card.CardText>
-          </Card.Container>
-        </Grid.Col>
-      </Grid.Row>
+          <Grid.Col
+            value={{ xs: 12, md: 6, lg: 4 }}
+            offset={{ xs: 0, md: 0, lg: 0 }}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            flexDirection="column"
+          >
+            <Card.Container>
+              <Card.Img src="https://via.placeholder.com/300x350" />
+              <Card.CardText>
+                <Text as="h1" variant="paragraph1" textAlign={"center"}>
+                  PROJETO
+                </Text>
+              </Card.CardText>
+            </Card.Container>
+          </Grid.Col>
+        </Grid.Row>
+      </div>
     </Grid.Container>
   );
 };
